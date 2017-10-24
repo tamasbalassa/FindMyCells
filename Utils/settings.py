2b63e22d4f2b558043462b9144bcfc9d14339dc1
Tamas Balassa <tamasbalassa9@gmail.com>
2017-10-24 14:19:03 +0200

    Settings (parameters) file added

---
+++ Utils/settings.py
@@ -0,0 +1,13 @@
+def init():
+    global gWidth
+    gWidth = 1200
+    
+    global gHeight
+    gHeight = 832
+    
+    global gInputWidth
+    gInputWidth = 0
+    
+    global gInputHeight
+    gInputHeight = 0
+    
