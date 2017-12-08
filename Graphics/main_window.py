7b55453172e8e2a80b8e81326daa26cbb06f28b5
Tamas Balassa <tamasbalassa9@gmail.com>
2017-12-09 00:15:54 +0100

    Cleanup + GUI reorder + progress bar + message boxes
    Signed-off-by: Tamas Balassa <tamasbalassa9@gmail.com>

--- Graphics/main_window.py
+++ Graphics/main_window.py
@@ -2,15 +2,12 @@ import sys, os
 sys.path.append('../AI')
 sys.path.append('../Utils')
 import csv
-#from PyQt5.QtWidgets import QWidget, QToolTip, QPushButton, QApplication, QFileDialog, QFormLayout, QHBoxLayout, QGroupBox, QVBoxLayout, QScrollBar
-import PyQt5.QtCore as qc
-from PyQt5.QtCore import Qt
 import PyQt5.QtWidgets as wdg
-from PyQt5.QtGui import QFont
 import PyQt5.QtGui as qg
+from PyQt5.QtCore import Qt
 from predict_image_DIGITS import predict_one, predict_all
-from imageUtils import switch_background, drawRect, set_image, load_images_from_dir, scroll_image
-from aiUtils import change_params_in_deploy
+from image_utils import switch_background, drawRect, set_image, load_images_from_dir, scroll_image
+from ai_utils import change_params_in_deploy
 import settings
 
 
@@ -32,8 +29,8 @@ class MainWindow(wdg.QWidget):
         
     def initUI(self):
         
-        wdg.QToolTip.setFont(QFont('SansSerif', 10))
-        self.setWindowTitle('FindMyCell')
+        wdg.QToolTip.setFont(qg.QFont('SansSerif', 10))
+        self.setWindowTitle('FindMyCells')
         
         hbox = wdg.QHBoxLayout()
 
@@ -42,48 +39,31 @@ class MainWindow(wdg.QWidget):
         groupBox1 = wdg.QGroupBox("Menu", self)
         vbox = wdg.QVBoxLayout(groupBox1)
         
-        vbox.addSpacing(30)
+        vbox.addSpacing(80)
         
-        # LOAD AND PREDICT SECTION
-        groupBox1_a = wdg.QGroupBox("L&P", self)
-        vbox.addWidget(groupBox1_a)
-        vbox1_a = wdg.QVBoxLayout(groupBox1_a)
-        vbox1_a.setSpacing(0)
+        # INPUT SECTION
+        groupBox1_input = wdg.QGroupBox("INPUT",self)
+        vbox.addWidget(groupBox1_input)
+        vbox1_input = wdg.QVBoxLayout(groupBox1_input)
+        vbox1_input.setContentsMargins(10, 5, 10, 5)        
+        vbox1_input.setSpacing(0)
         
-        # load button
+        # load button        
+        vbox1_input.addSpacing(10)
         load_btn = wdg.QPushButton('Load', self)
         load_btn.clicked.connect(self.loadbutton)
-        vbox1_a.addWidget(load_btn, 1, Qt.AlignTop)
-        vbox1_a.addSpacing(5)
+        vbox1_input.addWidget(load_btn, 1, Qt.AlignTop)
+        vbox1_input.addSpacing(10)
+                        
+        vbox.addSpacing(120)
         
-        # select image type text
-        select_img_type = wdg.QLabel()
-        select_img_type.setText("Image Type:")
-        vbox1_a.addWidget(select_img_type, 1)
-        
-        # select image type combobutton
-        self.combo_imgtype = wdg.QComboBox(self)
-        self.combo_imgtype.addItems(["jpg", "jpeg", "tif"])
-        vbox1_a.addWidget(self.combo_imgtype, 1)
-        vbox1_a.addSpacing(40)
-        
-        # predict button
-        predict_btn = wdg.QPushButton('Predict', self)
-        predict_btn.clicked.connect(self.predictbutton)
-        vbox1_a.addWidget(predict_btn, 1, Qt.AlignTop)
-        
-        #spacer
-        #verticalSpacer = wdg.QSpacerItem(40, 60, wdg.QSizePolicy.Minimum, wdg.QSizePolicy.Expanding)
-        #vbox1_a.addItem(verticalSpacer)        
-         
-        vbox.addSpacing(50)
-        
-        # UTILS SECTION
-        groupBox1_utils = wdg.QGroupBox("UTILS", self)
+        # SETTINGS SECTION
+        groupBox1_utils = wdg.QGroupBox("SETTINGS", self)
         vbox.addWidget(groupBox1_utils)
         vbox1_utils = wdg.QVBoxLayout(groupBox1_utils)
         vbox1_utils.setContentsMargins(10, 5, 10, 5)
         vbox1_utils.setSpacing(0)
+        vbox1_utils.addSpacing(10)
         
         # switch model text
         switch_model_text = wdg.QLabel()
@@ -116,9 +96,9 @@ class MainWindow(wdg.QWidget):
         vbox1_utils.addSpacing(40)
                 
         # select cvg_threshold text
-        select_img_type = wdg.QLabel()
-        select_img_type.setText("Cvg_threshold:")
-        vbox1_utils.addWidget(select_img_type, 1)   
+        cvg_threshold_text = wdg.QLabel()
+        cvg_threshold_text.setText("Coverage threshold:")
+        vbox1_utils.addWidget(cvg_threshold_text, 1)   
         
         # select cvg_threshold textfield
         self.cvg_threshold = wdg.QLineEdit(self)
@@ -127,9 +107,9 @@ class MainWindow(wdg.QWidget):
         vbox1_utils.addSpacing(5)
         
         # select rect_threshold text
-        select_img_type = wdg.QLabel()
-        select_img_type.setText("Rect_threshold:")
-        vbox1_utils.addWidget(select_img_type, 1)   
+        rect_threshold_text = wdg.QLabel()
+        rect_threshold_text.setText("Rectangle threshold:")
+        vbox1_utils.addWidget(rect_threshold_text, 1)   
         
         # select rect_threshold textfield
         self.rect_threshold = wdg.QLineEdit(self)
@@ -146,42 +126,44 @@ class MainWindow(wdg.QWidget):
         #verticalSpacer = wdg.QSpacerItem(40, 60, wdg.QSizePolicy.Minimum, wdg.QSizePolicy.Expanding)
         #vbox1_utils.addItem(verticalSpacer)
                 
-        vbox.addSpacing(50)
+        vbox.addSpacing(100)
         
-        # OUTPUT GROUP        
-        flay_output = wdg.QFormLayout()
+        # OUTPUT SECTION
         
         groupBox1_output = wdg.QGroupBox("OUTPUT", self)
         vbox.addWidget(groupBox1_output)
         vbox1_output = wdg.QVBoxLayout(groupBox1_output)
+        vbox1_output.addSpacing(10)        
+        
+        # predict button
+        predict_btn = wdg.QPushButton('Predict', self)
+        predict_btn.clicked.connect(self.predictbutton)
+        vbox1_output.addWidget(predict_btn, 1, Qt.AlignTop)
+        vbox1_output.addSpacing(15)
         
         # statistics button
         stat_btn = wdg.QPushButton('Create stats', self)
         stat_btn.clicked.connect(self.statisticsbutton)
-        flay_output.addRow(stat_btn)
         vbox1_output.addWidget(stat_btn, 1)
+        vbox1_output.addSpacing(10)
         
-        #spacer
-        #verticalSpacer = wdg.QSpacerItem(40, 40, wdg.QSizePolicy.Minimum, wdg.QSizePolicy.Expanding)
-        #vbox1_output.addItem(verticalSpacer)
         
         vbox.addSpacing(180)
-        
-        #vbox.addLayout(flay, 1)        
+               
         hbox.addWidget(groupBox1, 1)
         
         # menu group ends here        
 # =============================================================================
         
         # image group starts here
-        vbox2 = wdg.QHBoxLayout()
-        groupBox2 = wdg.QGroupBox("Image", self)
+        vbox2 = wdg.QHBoxLayout() # full Image Layout
+        groupBox2 = wdg.QGroupBox("Image", self) # group box with the image
            
         vbox3 = wdg.QHBoxLayout(groupBox2)        
         vbox2.addWidget(groupBox2)
         hbox.addLayout(vbox2, 10)        
         
-        self.vbox = vbox3 # need this to be able to switch the image        
+        self.vbox = vbox3 # to be able to switch the image        
         
         # defaul background 
         pixmap = set_image(self, '..\Utils\FMC.png', mode='Single')
@@ -190,9 +172,13 @@ class MainWindow(wdg.QWidget):
         # scrollbar
         self.scrollbar = wdg.QScrollBar()
         self.setSliderMax(1)
-# FUTURE RELEASE
-#        self.scrollbar.sliderMoved.connect(self.scrollbar.setValue)
-        vbox2.addWidget(self.scrollbar, 1)        
+                        
+        self.progress = wdg.QProgressBar()
+        self.progress.setOrientation(Qt.Vertical)
+        self.progress.setValue(0)
+
+        vbox2.addWidget(self.scrollbar, 1)     
+        vbox2.addWidget(self.progress, 1) 
 
         # image group ends here
 # =============================================================================   
@@ -201,12 +187,13 @@ class MainWindow(wdg.QWidget):
         self.show()
         
     def loadbutton(self):
-        
-        global imgdir
         self.global_image_path_list = []
+        self.wheelValue = -1
 
-        imgdir = str(wdg.QFileDialog.getExistingDirectory(self, "Select Directory"))
-        load_images_from_dir(self, imgdir)
+        file_name = wdg.QFileDialog()
+        names = file_name.getOpenFileNames(self, "Open files","","Images (*.jpg *.jpeg *.png *.tif *.tiff)")        
+        
+        load_images_from_dir(self, names[0])
         
         self.setSliderMax(len(self.global_image_path_list))
         
@@ -217,19 +204,22 @@ class MainWindow(wdg.QWidget):
         
         pixmapToPredict = self.vbox.itemAt(0).widget().pixmap()        
         if (type(self.global_image_path_list) is list):
-            rectangles = predict_one(self, [self.global_image_path_list[self.wheelValue]], True) # TODO parameter
-            drawRect(self, pixmapToPredict, rectangles)
+            rectangles = predict_one(self, [self.global_image_path_list[self.wheelValue]], False) # TODO parameter
+            drawRect(pixmapToPredict, rectangles)
         
             pm = set_image(self, pixmapToPredict, mode='Pixmap')
             switch_background(self, pm)
+            
+        self.progress.setValue(0)
        
     def statisticsbutton(self):
-        global imgdir
         
-        rectangles = predict_all(self, self.global_image_path_list, True)
+        self.progress.setValue(0)
         
-        with open('eggs.csv', 'wb') as csvfile:
-            fieldnames = ['Image Number', 'Cell ID', 'x1', 'y1', 'x2', 'y2']
+        rectangles = predict_all(self, self.global_image_path_list, False)
+        
+        with open('result.csv', 'wb') as csvfile:
+            fieldnames = ['Image Name', 'Cell ID', 'x1', 'y1', 'x2', 'y2']
             writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)        
             writer.writeheader()
             
@@ -238,12 +228,16 @@ class MainWindow(wdg.QWidget):
             for row in rectangles:
                 if row[-1] != imgID:
                     imgID = row[-1]
-                    cellID = 0
+                    cellID = 1
                 else:
                     cellID += 1
                 imgName = self.global_image_path_list[row[-1]].split(os.sep)
                 
-                writer.writerow({'Image Number': imgName[-1], 'Cell ID': cellID, 'x1': int(row[0]), 'y1': int(row[1]), 'x2': int(row[2]), 'y2': int(row[3])})
+                writer.writerow({'Image Name': imgName[-1], 'Cell ID': cellID, 'x1': int(row[0]), 'y1': int(row[1]), 'x2': int(row[2]), 'y2': int(row[3])})
+                
+                
+        self.progress.setValue(0)
+        wdg.QMessageBox.about(self, "Prediction result", "The result file is placed in the Graphics/ folder.")
         
     def settresholdbutton(self):
         change_params_in_deploy(self)              
@@ -251,6 +245,7 @@ class MainWindow(wdg.QWidget):
     def switchmodelbutton(self):
         self.model = '../AI/models/' + self.combo_model.currentText() + '.caffemodel'
         self.architecture = '../AI/models/' + self.combo_arch.currentText() + '.prototxt'
+        wdg.QMessageBox.about(self, "Model change", "The model files have been changed.")
       
     def model_combo_contents(self):
         dirpath = '../AI/models/'
@@ -275,13 +270,6 @@ class MainWindow(wdg.QWidget):
         
         if (type(self.global_image_path_list) is list) and len(self.global_image_path_list) > 0:
             self.global_image_path_list = scroll_image(self, self.global_image_path_list)
-# FUTURE RELEASE
-# =============================================================================
-#         self.setSliderValue(self.wheelValue)
-# 
-#     def setSliderValue(self, value):
-#         self.scrollbar.setValue(value)
-# =============================================================================
 
     def setSliderMax(self, maxValue):
         self.scrollbar.setMaximum(maxValue-1)
